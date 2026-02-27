2f0e98e4f612f0e95fb796d750ae580bd19a470a
Majncz42 <mikulastichy009@gmail.com>
2026-02-27 09:53:13 +0100

    20.2.2026

---
+++ 1_Lekce/script.js
@@ -0,0 +1,46 @@
+let userName = "Mike";
+console.log(userName)
+
+userName = "Jonáš";
+console.log(userName)
+
+console.log(7+4);
+console.log("7"+"4");
+console.log("A"+"B");
+console.log("7"+4);
+
+let age = 17;
+console.log(age > 20);
+
+let text = "Text";
+console.log(typeof text);
+
+let hodnota = null;
+console.log(hodnota);
+
+let cisloA = 11;
+let cisloB = 5;
+console.log(cisloA + cisloB);
+console.log(cisloA - cisloB);
+console.log(cisloA * cisloB);
+console.log(cisloA / cisloB);
+// Umocňování **
+console.log(cisloA ** cisloB);
+// Zbytek %
+console.log(cisloA % cisloB);
+
+
+console.log((cisloA + cisloB) * cisloB);
+// Aktualizace proměnné
+
+let number = 18;
+number = number + 1;
+number += 1;
+number -= 5;
+number *= 4;
+number /= 3;
+number %= 6;
+number++;
+number--;
+
+console.log(number);
---
+++ 2.lekce/script.js
@@ -0,0 +1,17 @@
+let userName = "Jacob";
+console.log(userName);
+let Jacobage = 13;
+console.log(Jacobage)
+
+
+Name = "John";
+console.log(Name);
+let Johnage = 67
+console.log(Johnage)
+
+number = Jacobage + Johnage
+console.log(number)
+
+
+let text = "Johnovi je 67 a Jacobovi je 13." + number
+console.log(text)
