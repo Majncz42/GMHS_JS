57bd67fe3b11e29ad194da5d284aca1761146285
Majncz42 <mikulastichy009@gmail.com>
2026-04-10 10:07:16 +0200

    Update script.js
    10.04

--- 13.03/script.js
+++ 13.03/script.js
@@ -3,27 +3,26 @@ let age = 14
 if (age < 3) {
  console.log ("Neplatíte nic.")   
 }
- else {
-    if (3 <= age <= 12){ 
-        console.log ("Zaplaťte 5 dolarů.")
-    }
-        
-    else {
-      if (13<=age<=17) {
-        console.log("zaplaťte 7 dolarů.")
-      } else {
-        
-      }
-        
-            
-        }
-        
-    }
-        
+ else if    (3 <= age <= 12)
+      { 
+         console.log ("Zaplaťte 5 dolarů.")
+      } 
+      
+     
     
+
     
+ else if (13 <= age <= 17)   {
+   console.log ("Zaplaťte 7 dolarů.")
+ }
    
+else if (18 <= age <= 64){
+   console.log ("Zaplaťte 10 dolarů.")
+}
            
+else if (65 <= age ) {
+   console.log ("Zaplaťte 6 dolarů.")
+}
          
        
     
